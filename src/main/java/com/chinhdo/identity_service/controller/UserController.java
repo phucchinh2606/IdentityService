dccfaa4e5452704bef9720e0b5f642bb45319f45
phucchinh2606 <chinhdo266@gmail.com>
2025-03-27 03:29:09 +0700

    hoan thanh crud api users

--- src/main/java/com/chinhdo/identity_service/controller/UserController.java
+++ src/main/java/com/chinhdo/identity_service/controller/UserController.java
@@ -1,20 +1,43 @@
 package com.chinhdo.identity_service.controller;
 
 import com.chinhdo.identity_service.dto.request.UserCreationRequest;
+import com.chinhdo.identity_service.dto.request.UserUpdateRequest;
 import com.chinhdo.identity_service.entity.User;
 import com.chinhdo.identity_service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/users") //ko can phai khai bao endpoint khi co annotation nay
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/users") //endpoint
+    @PostMapping //endpoint
     User createUser(@RequestBody UserCreationRequest request){//map data body vao UsercreationRequest
         return userService.createUser(request);
+    }
+
+    @GetMapping
+    List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId){ //map userId endpoint vao bien parameter
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request){
+        return userService.updateUser(userId,request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String deleteUser(@PathVariable String userId){
+        userService.deleteUser(userId);
+        return "User has been deleted =)) ";
     }
 }
